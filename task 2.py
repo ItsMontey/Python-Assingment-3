79d284d155e8629b659391404fde7f4a305c1215
itsMontey <146916924+ItsMontey@users.noreply.github.com>
2025-09-27 00:18:05 +0530

    Add files via upload

---
+++ task 2.py
@@ -0,0 +1,11 @@
+import math
+def calculations():
+    number = input("Enter a Number: ")
+    number = float(number)
+
+    print(f'Square root: {math.sqrt(number)}')
+    print(f'Logarithm: {math.log(number)}')
+    print(f'Sine: {math.sin(number)}')
+
+
+calculations()
